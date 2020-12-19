d7aa235c24c76eaa6c69aa3d706a379db5dbfa4b
selvakumar <selvakumarappadurai93@gmail.com>
2020-12-19 23:25:35 +0530

    1)Changed theme color 2)Changed from accordin to custom card for project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,32 +18,32 @@ export class AppComponent {
   projects = [
     {
       title: 'Commercial Agent Front End (CAFE)',
-      description: 'My part is to develop, enhance and do modifications in frontend using React Js & backend webapi in .net core.',
+      content: 'My part is to develop, enhance and do modifications in frontend using React Js & backend webapi in .net core.',
       technologies: ['React Js', 'Dotnet Core']
     },
     {
       title: 'Merchant Wallet Interface',
-      description: 'My role involves developing web api services (ASP.NET - MVC) and creating tables, stored procedures (SQL Server)',
+      content: 'My role involves developing web api services (ASP.NET - MVC) and creating tables, stored procedures (SQL Server)',
       technologies: ['ASP.NET - MVC', 'SQL Server']
     },
     {
       title: 'CBD Refunds',
-      description: 'My role is developing web API service methods, also responsible for designing UI using aspx pages with CSS, Bootstrap, jQuery and integration.',
+      content: 'My role is developing web API service methods, also responsible for designing UI using aspx pages with CSS, Bootstrap, jQuery and integration.',
       technologies: ['ASP.NET', 'CSS', 'Bootstrap', 'jQuery']
     },
     {
       title: 'Cash Online Virtual Wallet',
-      description: 'I am responsible for developing web API service methods and to be consumed in client side. Developed client side (front-end) in ASP.Net MVC and UI designing CSS, Bootstrap, jQuery and integration.',
+      content: 'I am responsible for developing web API service methods and to be consumed in client side. Developed client side (front-end) in ASP.Net MVC and UI designing CSS, Bootstrap, jQuery and integration.',
       technologies: ['ASP.NET - MVC', 'CSS', 'Bootstrap', 'jQuery', 'SQL Server']
     },
     {
       title: 'Merchant Wallet Back office',
-      description: 'My work involved consuming service and integrating the data with UI, designing UI and dealing with SP (Stored Proc) in SQL.',
+      content: 'My work involved consuming service and integrating the data with UI, designing UI and dealing with SP (Stored Proc) in SQL.',
       technologies: ['ASP.NET - MVC', 'CSS', 'Bootstrap', 'jQuery', 'SQL Server']
     },
     {
       title: 'ISRM Qualys',
-      description: 'Accountable for enhancements, bug fixes and testing.',
+      content: 'Accountable for enhancements, bug fixes and testing.',
       technologies: ['ASP.NET - MVC']
     }];
 }
